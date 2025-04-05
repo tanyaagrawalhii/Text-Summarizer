30433c9fe5a6e8804f2f523af8e397526d232ba4
tanyaagrawalhii <nat.agra7@gmail.com>
2025-04-06 00:00:41 +0530

    ignore

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# from textSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from textSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 # from textSummarizer.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 # from textSummarizer.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 # from textSummarizer.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
@@ -6,15 +6,15 @@
 from textSummarizer.logging import logger
 
 logger.info("Welcome to our custom logging")
-# STAGE_NAME = "Data Ingestion stage"
-# try:
-#    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
-#    data_ingestion = DataIngestionTrainingPipeline()
-#    data_ingestion.main()
-#    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
-# except Exception as e:
-#         logger.exception(e)
-#         raise e
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
 
 
 
